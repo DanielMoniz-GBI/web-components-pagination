ca483e4f65fe4043cfe61721b67ab6ba9d8a8717
Daniel Moniz <daniel.moniz@groupbyinc.com>
2019-04-10 12:04:23 -0400

    Add sinple spinner.

--- client.js
+++ client.js
@@ -6,52 +6,66 @@
 //       "X-API-Key": "44f77fe0"
 //     }
 //   })
+document.addEventListener('DOMContentLoaded', function() {
+  window.getProducts = getProducts
+  getProducts()
+  // .then(getProducts.bind(null, 2))
+  // .then(getProducts.bind(null, 3))
 
-getProducts()
-  .then(getProducts.bind(null, 2))
-  .then(getProducts.bind(null, 3))
-
-
-
-function getProducts(page = 1, pageSize = 60) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(window.mockData)
-    }, 1000)
+  document.addEventListener('gbi:pagination-update', (pageNum) => {
+    getProducts(pageNum)
   })
-  // .then(resp => resp.json())
-  .then(results => {
-    console.log(results);
-    clearResults()
-    const pageMin = (page - 1) * pageSize
-    const pageMax = page * pageSize
-    results.slice(pageMin, pageMax).forEach(product => {
-      const tile = createTile(product)
-      addTile(tile)
+
+
+
+  function getProducts(page = 1, pageSize = 10) {
+    waitForResults()
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(window.mockData)
+      }, 1000)
     })
-  })
-}
+    // .then(resp => resp.json())
+    .then(results => {
+      console.log(results);
+      clearResults()
+      const pageMin = (page - 1) * pageSize
+      const pageMax = page * pageSize
+      results.slice(pageMin, pageMax).forEach(product => {
+        const tile = createTile(product)
+        addTile(tile)
+      })
+    })
+  }
 
-function createTile(product) {
-  const tile = document.createElement('div')
-  tile.classList.add('tile')
+  function createTile(product) {
+    const tile = document.createElement('div')
+    tile.classList.add('tile')
 
-  const name = document.createElement('div')
-  name.innerText = product.name
-  tile.append(name)
-  const price = document.createElement('div')
-  price.innerText = 'For only: $' + product.price
-  tile.append(price)
+    const name = document.createElement('div')
+    name.innerText = product.name
+    tile.append(name)
+    const price = document.createElement('div')
+    price.innerText = 'For only: ' + product.price
+    tile.append(price)
 
-  return tile
-}
+    return tile
+  }
 
-function addTile(tile) {
-  const results = document.querySelector('main .results')
-  results.append(tile)
-}
+  function addTile(tile) {
+    const results = document.querySelector('main .results')
+    results.append(tile)
+  }
 
-function clearResults() {
-  const results = document.querySelector('main .results')
-  results.innerHTML = ''
-}
+  function clearResults() {
+    const results = document.querySelector('main .results')
+    results.innerHTML = ''
+  }
+
+  function waitForResults() {
+    const results = document.querySelector('main .results')
+    const spinner = document.createElement('img')
+    spinner.src = 'spinner.jpg'
+    results.prepend(spinner)
+  }
+})
