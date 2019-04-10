1bf3da6447851f52d5e3a46b6a673551ed5009c3
Daniel Moniz <daniel.moniz@groupbyinc.com>
2019-04-10 16:33:53 -0400

    Allow for pageNums based on passed num-pages attr.

--- component.js
+++ component.js
@@ -17,12 +17,23 @@ function defineComponents() {
       if (!numPages) { numPages = 1 }
       numPages = parseInt(numPages)
       console.log(numPages);
-      this.innerHTML = `<span class="links"><< < 1 2 3 4 5 > >></span>`
+      this.innerHTML = `<span class="links"><< < ${this.getPageNumbers(numPages)} > >></span>`
 
     }
 
     attributeChangedCallback(name, oldValue, newValue) {
       console.log('in attributeChangedCallback');
+    }
+
+    getPageNumbers(numPages) {
+      if (numPages === 0) {
+        return ''
+      }
+      const nums = []
+      for (let i = 1; i <= numPages; i++) {
+        nums.push(i)
+      }
+      return nums.join(" ")
     }
   })
 }
